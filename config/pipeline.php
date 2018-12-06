46de0b8c37b53c3faf199d0939262ee4ac31d3dd
Jonathan Beliën <jbe@geo6.be>
2018-12-06 14:36:49 +0100

    Add BaseUrlMiddleware

--- config/pipeline.php
+++ config/pipeline.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Middleware\DbAdapterMiddleware;
+use Blast\BaseUrl\BaseUrlMiddleware;
 use Psr\Container\ContainerInterface;
 use Zend\Expressive\Application;
 use Zend\Expressive\Handler\NotFoundHandler;
@@ -45,6 +46,7 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
 
     // Register the routing middleware in the middleware pipeline.
     // This middleware registers the Zend\Expressive\Router\RouteResult request attribute.
+    $app->pipe(BaseUrlMiddleware::class);
     $app->pipe(RouteMiddleware::class);
 
     // The following handle routing failures for common conditions:
