f6b76aed85ff43ff480c049a7057d4fbc7fcb1fe
Jonathan Beliën <jbe@geo6.be>
2018-12-13 15:29:39 +0100

    Switch to MapFile library v2

--- src/Script/Map/MapFile.php
+++ src/Script/Map/MapFile.php
@@ -12,263 +12,272 @@ class MapFile
     /** @var string */
     private $slug;
 
-    /** @var \MapFile\map */
-    private $mapfile;
+    /** @var \MapFile\Model\Map */
+    private $map;
 
     public function __construct(string $key, string $slug, array $extent)
     {
         $this->key = $key;
         $this->slug = $slug;
 
-        $this->mapfile = new \MapFile\map();
+        $this->map = new \MapFile\Model\Map();
 
-        $this->mapfile->setFontSet(realpath('data/maps/fonts.txt') ?? '');
-        $this->mapfile->setSymbolSet(realpath('data/maps/symbols.txt') ?? '');
-        $this->mapfile->setSize(400, 400);
-        $this->mapfile->setImageColor(148, 206, 244);
-        $this->mapfile->name = sprintf('%s_%s', $this->key, $this->slug);
-        $this->mapfile->projection = 'EPSG:3857';
+        $this->map->config = [
+            // 'MS_ERRORFILE' => '/tmp/mapserver.log',
+        ];
+        // $this->map->debug = 'on';
+        $this->map->fontset = realpath('data/maps/fonts.txt') ?? '';
+        $this->map->symbolset = realpath('data/maps/symbols.txt') ?? '';
+        $this->map->size = [400, 400];
+        $this->map->imagecolor = [148, 206, 244];
+        $this->map->name = sprintf('%s_%s', $this->key, $this->slug);
+        $this->map->projection = 'epsg:3857';
 
         $dx = $extent['maxx'] - $extent['minx'];
         $dy = $extent['maxy'] - $extent['miny'];
 
-        $this->mapfile->setExtent(
+        $this->map->extent = [
             $extent['minx'] - (0.05 * $dx),
             $extent['miny'] - (0.05 * $dy),
             $extent['maxx'] + (0.05 * $dx),
             $extent['maxy'] + (0.05 * $dy)
-        );
+        ];
     }
 
-    public function addScalebar() : \MapFile\scalebar
+    public function addScalebar() : \MapFile\Model\Scalebar
     {
-        $this->mapfile->scalebar->units = \MapFile\scalebar::UNITS_KILOMETERS;
-        $this->mapfile->scalebar->setOutlineColor(0, 0, 0);
-        $this->mapfile->scalebar->label->type = \MapFile\label::TYPE_TRUETYPE;
-        $this->mapfile->scalebar->label->font = 'dejavusans';
-        $this->mapfile->scalebar->label->size = 7;
+        $this->map->scalebar = new \MapFile\Model\Scalebar();
 
-        return $this->mapfile->scalebar;
+        $this->map->scalebar->units = 'kilometers';
+        $this->map->scalebar->outlinecolor = [0, 0, 0];
+
+        $this->map->scalebar->label = new \MapFile\Model\Label();
+
+        $this->map->scalebar->label->type = 'truetype';
+        $this->map->scalebar->label->font = 'dejavusans';
+        $this->map->scalebar->label->size = 7;
+
+        return $this->map->scalebar;
     }
 
-    public function addLayerLand() : \MapFile\layer
+    public function addLayerLand() : \MapFile\Model\Layer
     {
-        $layer = new \MapFile\layer();
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = 'land';
-        $layer->projection = 'EPSG:3857';
-        $layer->type = \MapFile\layer::TYPE_POLYGON;
+        $layer->projection = 'epsg:3857';
+        $layer->type = 'polygon';
         $layer->data = preg_replace('/\.shp$/', '', realpath('data/openstreetmap/simplified-land-polygons-complete-3857/simplified_land_polygons.shp'));
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor(242, 239, 233);
-        $style->setOutlineColor(80, 80, 80);
+        $style->color = [242, 239, 233];
+        $style->outlinecolor = [80, 80, 80];
         $style->width = 0.5;
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
 
-    public function addLayerLanduseGreen() : \MapFile\layer
+    public function addLayerLanduseGreen() : \MapFile\Model\Layer
     {
-        $layer = new \MapFile\layer();
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = 'landusegreen';
-        $layer->projection = 'EPSG:4326';
-        $layer->type = \MapFile\layer::TYPE_POLYGON;
+        $layer->projection = 'epsg:4326';
+        $layer->type = 'polygon';
         $layer->data = preg_replace('/\.shp$/', '', realpath('data/openstreetmap/gis_osm_landuse_a_free_1.shp'));
         $layer->filter = '("[fclass]"=\'forest\' or "[fclass]"=\'park\' or "[fclass]"=\'nature_reserve\')';
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor(172, 234, 164);
+        $style->color = [172, 234, 164];
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
 
-    public function addLayerWater() : \MapFile\layer
+    public function addLayerWater() : \MapFile\Model\Layer
     {
-        $layer = new \MapFile\layer();
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = 'water';
-        $layer->projection = 'EPSG:4326';
-        $layer->type = \MapFile\layer::TYPE_POLYGON;
+        $layer->projection = 'epsg:4326';
+        $layer->type = 'polygon';
         $layer->data = preg_replace('/\.shp$/', '', realpath('data/openstreetmap/gis_osm_water_a_free_1.shp'));
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor(148, 206, 244);
+        $style->color = [148, 206, 244];
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
 
-    public function addLayerRoad() : \MapFile\layer
+    public function addLayerRoad() : \MapFile\Model\Layer
     {
-        $layer = new \MapFile\layer();
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = 'majorroad';
-        $layer->projection = 'EPSG:4326';
-        $layer->type = \MapFile\layer::TYPE_LINE;
+        $layer->projection = 'epsg:4326';
+        $layer->type = 'line';
         $layer->data = preg_replace('/\.shp$/', '', realpath('data/openstreetmap/gis_osm_roads_free_1.shp'));
         $layer->filter = '("[fclass]"=\'motorway\' or "[fclass]"=\'trunk\' or "[fclass]"=\'primary\')';
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
         $class->expression = '("[fclass]" = \'motorway\')';
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor(150, 150, 150);
+        $style->color = [150, 150, 150];
         $style->width = 2;
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
         $class->expression = '("[fclass]"=\'trunk\' or "[fclass]"=\'primary\')';
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor(190, 190, 190);
+        $style->color = [190, 190, 190];
         $style->width = 1;
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
 
-    public function addLayerProvince() : \MapFile\layer
+    public function addLayerProvince() : \MapFile\Model\Layer
     {
-        $layer = new \MapFile\layer();
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = 'province';
-        $layer->projection = 'EPSG:31370';
-        $layer->type = \MapFile\layer::TYPE_POLYGON;
-        $layer->connectiontype = \MapFile\layer::CONNECTIONTYPE_OGR;
+        $layer->projection = 'epsg:31370';
+        $layer->type = 'polygon';
+        $layer->connectiontype = 'ogr';
         $layer->connection = realpath('data/statbel/sh_statbel_province.sqlite');
         $layer->data = 'sh_statbel_province';
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setOutlineColor(80, 80, 80);
+        $style->outlinecolor = [80, 80, 80];
         $style->width = 2;
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
 
-    public function addLayerCity(bool $city = true, bool $town = false) : \MapFile\layer
+    public function addLayerCity(bool $city = true, bool $town = false) : \MapFile\Model\Layer
     {
-        $layer = new \MapFile\layer();
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = 'city';
-        $layer->projection = 'EPSG:4326';
-        $layer->type = \MapFile\layer::TYPE_POINT;
+        $layer->projection = 'epsg:4326';
+        $layer->type = 'point';
         $layer->data = preg_replace('/\.shp$/', '', realpath('data/openstreetmap/gis_osm_places_free_1.shp'));
         $layer->labelitem = 'name';
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
         $class->expression = '("[fclass]" = "national_capital")';
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor(80, 80, 80);
-        $style->symbolname = 'circle';
+        $style->color = [80, 80, 80];
+        $style->symbol = 'circle';
         $style->size = 8;
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $label = new \MapFile\label();
+        $label = new \MapFile\Model\Label();
 
-        $label->type = \MapFile\label::TYPE_TRUETYPE;
+        $label->type = 'truetype';
         $label->font = 'dejavusans';
         $label->size = 8;
-        // $label->buffer = 10;
-        $label->setColor(80, 80, 80);
-        $label->setOutlineColor(255, 255, 255);
-        $label->position = \MapFile\label::POSITION_AUTO;
-        $label->align = \MapFile\label::ALIGN_CENTER;
-        // $label->wrap = 47;
+        $label->buffer = 10;
+        $label->color = [80, 80, 80];
+        $label->outlinecolor = [255, 255, 255];
+        $label->position = 'auto';
+        $label->align = 'center';
+        $label->wrap = chr(47);
 
-        $class->addLabel($label);
+        $class->label->add($label);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
         if ($city === true) {
-            $class = new \MapFile\layerclass();
+            $class = new \MapFile\Model\LayerClass();
 
             $class->expression = '("[fclass]" = "city")';
 
-            $style = new \MapFile\style();
+            $style = new \MapFile\Model\Style();
 
-            $style->setColor(80, 80, 80);
-            $style->symbolname = 'circle';
+            $style->color = [80, 80, 80];
+            $style->symbol = 'circle';
             $style->size = 5;
 
-            $class->addStyle($style);
+            $class->style->add($style);
 
-            $class->addLabel($label);
+            $class->label->add($label);
 
-            $layer->addClass($class);
+            $layer->class->add($class);
         }
 
         if ($town === true) {
-            $class = new \MapFile\layerclass();
+            $class = new \MapFile\Model\LayerClass();
 
             $class->expression = '("[fclass]" = "town")';
 
-            $style = new \MapFile\style();
+            $style = new \MapFile\Model\Style();
 
-            $style->setColor(80, 80, 80);
-            $style->symbolname = 'circle';
+            $style->color = [80, 80, 80];
+            $style->symbol = 'circle';
             $style->size = 5;
 
-            $class->addStyle($style);
+            $class->style->add($style);
 
-            $class->addLabel($label);
+            $class->label->add($label);
 
-            $layer->addClass($class);
+            $layer->class->add($class);
         }
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
@@ -281,29 +290,29 @@ class MapFile
         string $password,
         array $municipalities,
         array $color
-    ) : \MapFile\layer {
-        $layer = new \MapFile\layer();
+    ) : \MapFile\Model\Layer {
+        $layer = new \MapFile\Model\Layer();
 
         $layer->name = $this->slug;
-        $layer->projection = 'EPSG:4326';
-        $layer->connectiontype = \MapFile\layer::CONNECTIONTYPE_POSTGIS;
-        $layer->type = \MapFile\layer::TYPE_POLYGON;
+        $layer->projection = 'epsg:4326';
+        $layer->connectiontype = 'postgis';
+        $layer->type = 'polygon';
 
         $layer->connection = 'host='.$host.' port='.$port.' dbname='.$dbname.' user='.$user.' password='.$password;
         $layer->data = 'the_geom from (SELECT nis5, the_geog::geometry AS the_geom FROM municipalities WHERE nis5 IN('.implode(',', $municipalities).')) as subquery using unique nis5 using srid=4326';
 
-        $class = new \MapFile\layerclass();
+        $class = new \MapFile\Model\LayerClass();
 
-        $style = new \MapFile\style();
+        $style = new \MapFile\Model\Style();
 
-        $style->setColor($color[0], $color[1], $color[2]);
+        $style->color = $color;
         $style->opacity = 30;
 
-        $class->addStyle($style);
+        $class->style->add($style);
 
-        $layer->addClass($class);
+        $layer->class->add($class);
 
-        $this->mapfile->addLayer($layer);
+        $this->map->layer->add($layer);
 
         return $layer;
     }
@@ -316,8 +325,10 @@ class MapFile
             mkdir($directory, 0777, true);
         }
 
-        $this->mapfile->save(
-            sprintf('data/maps/%s/temp/%s.map', $this->key, $this->slug)
+        $mapfile = (new \MapFile\Writer\Map())->write($this->map);
+        file_put_contents(
+            sprintf('data/maps/%s/temp/%s.map', $this->key, $this->slug),
+            $mapfile
         );
     }
 }
