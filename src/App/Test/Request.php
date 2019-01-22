d8c389b013e4a3ef0963ecb18d7459d69169819c
Jonathan Beliën <jbe@geo6.be>
2019-01-22 13:40:20 +0100

    Handle better exception in test page

--- src/App/Test/Request.php
+++ src/App/Test/Request.php
@@ -18,6 +18,7 @@ class Request
             'query'    => null,
             'status'   => null,
             'response' => null,
+            'error'    => null,
         ];
 
         $root = 'http'.(isset($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['SERVER_NAME'].(!in_array($_SERVER['SERVER_PORT'], [80, 443], false) ? ':'.$_SERVER['SERVER_PORT'] : '');
@@ -25,27 +26,29 @@ class Request
         $basepath = $request->getAttribute(BaseUrlMiddleware::BASE_PATH);
         $query = rtrim($basepath, '/').$query;
 
-        $client = new Client([
-            'base_uri' => $root,
-            'timeout'  => 1.0,
-        ]);
+        try {
+            $client = new Client([
+                'base_uri' => $root,
+                'timeout'  => 1.0,
+            ]);
 
-        $response = $client->request('GET', $query, [
-            'http_errors' => false,
-            'on_stats'    => function (TransferStats $stats) use (&$result) {
-                $result['query'] = $stats->getEffectiveUri();
-                $result['time'] = $stats->getTransferTime() * 1000;
-            },
-        ]);
+            $response = $client->request('GET', $query, [
+                'http_errors' => false,
+                'on_stats'    => function (TransferStats $stats) use (&$result) {
+                    $result['query'] = $stats->getEffectiveUri();
+                    $result['time'] = $stats->getTransferTime() * 1000;
+                },
+            ]);
 
-        $result['status'] = $response->getStatusCode();
+            $result['status'] = $response->getStatusCode();
 
-        $json = json_decode((string) $response->getBody());
+            $json = json_decode((string) $response->getBody());
 
-        if (!is_null($json)) {
-            $result['response'] = json_encode($json, JSON_PRETTY_PRINT);
-        } else {
-            $result['response'] = (string) $response->getBody();
+            if (!is_null($json)) {
+                $result['response'] = json_encode($json, JSON_PRETTY_PRINT);
+            }
+        } catch (\Exception $exception) {
+            $result['error'] = $exception->getMessage();
         }
 
         return $result;
