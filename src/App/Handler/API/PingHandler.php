948c3b94c0bb7a32d1de5054662b46b9860035bd
jbelien <dev@jbelien.be>
2019-07-01 02:21:11 +0000

    Apply fixes from StyleCI

--- src/App/Handler/API/PingHandler.php
+++ src/App/Handler/API/PingHandler.php
@@ -8,8 +8,8 @@ use App\Middleware\TokenMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\JsonResponse;
 use function time;
+use Zend\Diactoros\Response\JsonResponse;
 
 class PingHandler implements RequestHandlerInterface
 {
