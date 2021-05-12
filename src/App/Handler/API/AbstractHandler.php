309d8d79ae972f66274f20d17cf6ba71319e39e6
Jonathan Beliën <jbe@geo6.be>
2021-05-12 15:23:23 +0200

    🚨 Apply PHPCS fixes

--- src/App/Handler/API/AbstractHandler.php
+++ src/App/Handler/API/AbstractHandler.php
@@ -14,16 +14,16 @@ abstract class AbstractHandler implements RequestHandlerInterface
 {
     protected static function response(ServerRequestInterface $request, array $data, int $status = 200): JsonResponse
     {
-      $token = $request->getAttribute(TokenMiddleware::TOKEN_ATTRIBUTE);
-      $quota = $request->getAttribute(QuotaMiddleware::QUOTA_ATTRIBUTE);
+        $token = $request->getAttribute(TokenMiddleware::TOKEN_ATTRIBUTE);
+        $quota = $request->getAttribute(QuotaMiddleware::QUOTA_ATTRIBUTE);
 
-      if ($token['debug'] === true) {
-          $data['token'] = $token;
-      }
-      if ($quota['debug'] === true) {
-          $data['quota'] = $quota;
-      }
+        if ($token['debug'] === true) {
+            $data['token'] = $token;
+        }
+        if ($quota['debug'] === true) {
+            $data['quota'] = $quota;
+        }
 
-      return new JsonResponse($data, $status);
+        return new JsonResponse($data, $status);
     }
 }
